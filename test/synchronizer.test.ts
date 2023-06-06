0b6a4f2acf1ad36bf1e27acbe09864cbf01bb66b
momo-p <contact@relay.akimitsu.dev>
2023-06-07 00:23:56 +0700

    test: add single command and no command test for simple synchronizer

--- test/synchronizer.test.ts
+++ test/synchronizer.test.ts
@@ -3,8 +3,48 @@ import AsyncLock = require("async-lock");
 import { SimpleSynchronizer } from "../src";
 
 describe("simple synchronizer test", () => {
+  it("single command executed.", async () => {
+    const commands = ["1.1"];
+    const executedCommands = [];
+    const lock = new AsyncLock();
+
+    const synchronizer = new SimpleSynchronizer({
+      commands,
+      batchSize: 2,
+    });
+    await synchronizer.execute({
+      executor: async (command: string) => {
+        lock.acquire("lock", (done: any) => {
+          executedCommands.push(command);
+          done();
+        });
+      },
+    });
+    assert.deepEqual(commands.sort(), executedCommands.sort());
+  });
+
   it("all the commands executed.", async () => {
     const commands = ["1.1", "1.2", "1.3", "1.4", "1.5"];
+    const executedCommands = [];
+    const lock = new AsyncLock();
+
+    const synchronizer = new SimpleSynchronizer({
+      commands,
+      batchSize: 2,
+    });
+    await synchronizer.execute({
+      executor: async (command: string) => {
+        lock.acquire("lock", (done: any) => {
+          executedCommands.push(command);
+          done();
+        });
+      },
+    });
+    assert.deepEqual(commands.sort(), executedCommands.sort());
+  });
+
+  it("no command executed.", async () => {
+    const commands = [];
     const executedCommands = [];
     const lock = new AsyncLock();
 
